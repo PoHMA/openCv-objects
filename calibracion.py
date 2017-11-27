ec88225a814bae94183c6749cae831559b99cf56
cristhianzb <cristhianzentenob@gmail.com>
2017-11-27 09:40:30 -0400

    comienzo calibracion de la camara

---
+++ calibracion.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+import glob
+
+#criterio de terminacion
+criterio=(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,30,0.001)
+
+
+#preparar los puntos de objeto
+
+
+#distorsion radial
+
+#distorsion tangencial
